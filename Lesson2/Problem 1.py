cac495163751c8fbf8465ea9b2006f1b523f69c9
Chompyraptor <104743441+Chompyraptor@users.noreply.github.com>
2022-09-08 10:07:57 +1200

    Add files via upload

---
+++ Lesson2/Problem 1.py
@@ -0,0 +1,11 @@
+# T R Hoshino
+# variable including variables
+
+# Problem 01
+# This program is designed to track and total goals scored by the RC 2nd XI for each game played.
+
+game1_Pats = 7
+game2_Coll = 5
+game3_wellHigh = 10
+teamTotal = game1_Pats + game2_Coll + game3_wellHigh
+print ("Total Goals Scored:",teamTotal)
---
+++ Lesson2/Problem 2.py
@@ -0,0 +1,15 @@
+# T R Hoshino
+# concatenating string data types in the output
+
+# Problem 02
+# This program is designed to track the value remaining on a gift card as it is used.
+
+Card_Balance = 50.00
+Purchase_01 = 35.00
+
+
+print ("Your card balance $" + str (Card_Balance))
+
+Card_Balance = Card_Balance - Purchase_01
+print ("purchase_01 $" + str (Purchase_01))
+print ("Your New Balance is $" + str (Card_Balance))
